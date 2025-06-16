7fa1d561294430a523f11406fcc2fcbe98b57a43
Caiooliveirabarreto <caiooliveirabarreto4@gmail.com>
2025-06-16 08:51:00 -0300

    Finalizando Exercicio

--- ExerciciosExtra/exec02/aluno.class.php
+++ ExerciciosExtra/exec02/aluno.class.php
@@ -6,32 +6,40 @@
         private $Nome;
         private $Media;
 
-        function __construct($nota_1,$nota_2, $Nome, $Media = 0){
-            if($nota_1 = 0 || $nota_1 < 0 || $nota_2 = 0 || $nota_2 < 0){
-                echo "Valor não válido, por favor insira de novo.";
-            }
-            else{
-                $this->nota1 = $nota_1;
-                $this->nota2 = $nota_2;
-            }
+        public function __construct($nota_1,$nota_2, $Nome, $Media = 0){
+            $this->nota1 = $nota_1;
+            $this->nota2 = $nota_2;
             $this->Nome = $Nome;
         }
     
-        function getNome(){
-            echo 'O nome do Aluno é: ' . $this->Nome . "<br>";
+        public function getNota1(){
+            return $this->nota1;
+            
+        }
+        public function getNota2(){
+            return $this->nota2;
+            
+        }
+        
+        public function getNome(){
+            return $this->Nome;
         }
 
-        function getMedia(){
-            $Media = ($this->nota1 + $this->nota1) / 2;
-            echo 'A média de nota foi: ' . $Media . "<br>";
+        public function getMedia(){
+            $nota1 = $this->getNota1();
+            $nota2 = $this->getNota2();
+            $Media = ($nota1+ $nota2) / 2;
+            return $Media;
+            
         }
 
-        function verificarSituacao(){
-            if ($Media >= 7) {
-                echo "Aprovado!" . "<br>";
+        public function verificarSituacao(){
+            $media = $this->getMedia();
+            if ($media >= 7) {
+                echo 'Aprovado!';
             }
             else{
-                echo 'Reprovado'. "<br>";
+                echo 'Reprovado!';            
             }
         }
         
--- ExerciciosExtra/exec02/exec02.php
+++ ExerciciosExtra/exec02/exec02.php
@@ -1,10 +1,10 @@
 <?php
     include_once 'aluno.class.php';
 
-    $aluno1 = new Aluno(6 , 8 , 'Heitor');
+    $aluno1 = new Aluno(5 , 8 , 'Heitor');
 
-    $aluno1->getMedia();
-    $aluno1->getNome();
+    echo $aluno1->getMedia() . '<br>';
+    echo $aluno1->getNome() . '<br>';
 
     $aluno1->verificarSituacao();
 
